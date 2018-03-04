912b161e69245adda399294a96c2f3cb478b6501
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2018-03-04 10:08:26 +0100

    Fix apache start

--- script.sh
+++ script.sh
@@ -5,5 +5,7 @@ if [ ! /etc/letsencrypt/live ]; then
   echo "Running certbot to retrieve certificates" 
   certbot --non-interactive --agree-tos --email $EMAIL --apache --domains $DOMAIN $ARGUMENT
 fi
-echo "Apache web server started and certificates retrieved" 
+echo "Apache web server started and certificates retrieved"
+
+tail -f /var/log/apache2/error.log
 
